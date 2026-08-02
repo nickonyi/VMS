c1d3d8455287f9ee9725ba31f00fefc0acded1d2
nickonyi <nickoluoch1130@gmail.com>
2026-08-02 22:41:36 +0300

    feat: integrate routing and update app structure
    - Added react-router for navigation between pages.
    - Updated App component to include navigation links.
    - Created Profile, Popeye, Spinach, and DefaultProfile components for dynamic content.
    - Implemented ErrorPage for handling non-existent routes.
    - Updated main entry point to use RouterProvider for routing setup.
    - Added Tailwind CSS and related dependencies for styling.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import Profile from "./Profile.jsx";
+import Popeye from "./Popeye.jsx";
+import Spinach from "./Spinach.jsx";
+import DefaultProfile from "./DefaultProfile.jsx";
+import ErrorPage from "./ErrorPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
